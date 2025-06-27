c15a221738925642d68c92f40069b684808bc477
pasindu1202 <pasindu1202@gmail.com>
2025-06-28 00:02:39 +0530

    Rechange Homepage

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import LoginForm from './LoginFrom/LoginFrom';
 import SignUp from './SignUpForm/SignUp';
 import Payment from './PaymentFrom/Payment';
 import Dashboard from './Dashboard/Dashboard';
+import HomePage from './HomePage/HomePage';
 
 
 
@@ -17,6 +18,7 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/payment" element={<Payment />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/hpage" element={<HomePage/>}/>
       </Routes>
     </BrowserRouter>
     
